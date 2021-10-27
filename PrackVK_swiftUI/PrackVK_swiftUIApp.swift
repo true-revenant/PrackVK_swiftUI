e9c538c2b8d2a3ed942de7b599c0a324cd38b458
Sergey B <badasyansl@yandex.ru>
2021-10-27 15:17:47 +0500

    created empty project

---
+++ PrackVK_swiftUI/PrackVK_swiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrackVK_swiftUIApp.swift
+//  PrackVK_swiftUI
+//
+//  Created by Сергей Бадасян on 27.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct PrackVK_swiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
